70c555a2b1af52bd34161da3328ffd00389cb6f5
Caleb Osorio <calebosorio0529@gmail.com>
2022-12-01 13:25:47 -0500

    added logic to sign in

---
+++ Senior Project/Structures/User.swift
@@ -0,0 +1,26 @@
+//
+//  NewUser.swift
+//  Senior Project
+//
+//  Created by 11k on 12/1/22.
+//
+
+import Foundation
+
+public struct NewUser{
+    
+    var username:String
+    var firstName:String
+    var lastName:String
+    var password:String
+    
+    init?(_ userName:String?, _ firstName:String?, _ lastName:String?, _ password:String?){
+        
+        guard let username = userName, let firstName = firstName, let lastName = lastName, let password = password else{return nil}
+        
+        self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
+        self.password = password
+    }
+}
