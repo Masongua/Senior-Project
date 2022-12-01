611354ef6dd43ae0ecabcf14beb43bc6dd25ac8d
Caleb Osorio <calebosorio0529@gmail.com>
2022-12-01 15:29:39 -0500

    added tab bar

---
+++ Senior Project/ViewControllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  Senior Project
+//
+//  Created by 11k on 12/1/22.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Senior Project/ViewControllers/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Senior Project
+//
+//  Created by 11k on 12/1/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
